ecd5e0d555b014d6320b42ff1cf2cf87428a4c76
devsungmin <kimsungmin.dev@gmail.com>
2022-11-14 00:23:19 +0900

    add: project init

---
+++ src/main/kotlin/dev/sungmin/price/RealEstateTransactionPriceApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.sungmin.price
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealEstateTransactionPriceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealEstateTransactionPriceApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/sungmin/price/RealEstateTransactionPriceApiApplicationTests.kt
@@ -0,0 +1,12 @@
+package dev.sungmin.price
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealEstateTransactionPriceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
